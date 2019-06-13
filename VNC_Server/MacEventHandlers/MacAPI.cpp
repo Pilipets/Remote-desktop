6f5dffea94f9fa680332274b99765f035b3003aa
Pilipets <glebon1802@gmail.com>
2019-06-13 17:58:31 +0300

    fixed errors with compilation

--- VNC_Server/MacEventHandlers/MacAPI.cpp
+++ VNC_Server/MacEventHandlers/MacAPI.cpp
@@ -1,19 +1,19 @@
-#include <ApplicationServices/ApplicationServices.h>
+//#include <ApplicationServices/ApplicationServices.h>
 #include "MacAPI.h"
 
 void MacApi::PressKey(quint16 unicode)
 {
-    CGEventRef downEvt = CGEventCreateKeyboardEvent(NULL, 0, true);
+    /*CGEventRef downEvt = CGEventCreateKeyboardEvent(NULL, 0, true);
     UniChar oneChar = unicode;
     CGEventKeyboardSetUnicodeString(downEvt,1,&oneChar);
-    CGEventPost(kCGAnnotatedSessionEventTap,downEvt);
+    CGEventPost(kCGAnnotatedSessionEventTap,downEvt);*/
 }
 
 
 void MacApi::ReleaseKey(quint16 unicode)
 {
-    CGEventRef upEvt = CGEventCreateKeyboardEvent(NULL, 0, false);
+    /*CGEventRef upEvt = CGEventCreateKeyboardEvent(NULL, 0, false);
     UniChar oneChar = unicode;
     CGEventKeyboardSetUnicodeString(upEvt,1,&oneChar);
-    CGEventPost(kCGAnnotatedSessionEventTap,upEvt);
+    CGEventPost(kCGAnnotatedSessionEventTap,upEvt);*/
 }
